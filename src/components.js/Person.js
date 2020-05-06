366150f5d2913f2e8fd99ceb8831453b044b55b5
Arsen2210 <arsmadanyan@gmail.com>
2020-05-06 16:26:49 +0400

    Second init

---
+++ src/components.js/Person.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function hello(){
+    return (
+        <div>
+            <h1>Hello World!!</h1>
+            <h2>Hello World!!</h2>
+            <h3>Hello World!!</h3>
+            <p>I am a React Developer</p>
+        </div>
+    );
+};
+
+
