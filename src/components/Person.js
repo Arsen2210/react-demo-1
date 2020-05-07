375fb07c177233ecc87f84ef7678edc1e0a37557
Arsen2210 <arsmadanyan@gmail.com>
2020-05-07 20:40:16 +0400

    Props3

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Person(props){
     return (
-        <div>
+        <div className="person">
             <h1>Hello World!!</h1>
             <h2>Hello World!!</h2>
             <h3>Hello World!!</h3>
