ca71671725401c98f04a610decc6f1c33ad98a1a
Arsen2210 <arsmadanyan@gmail.com>
2020-05-07 20:38:12 +0400

    Props2

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,7 +6,7 @@ export default function Person(props){
             <h1>Hello World!!</h1>
             <h2>Hello World!!</h2>
             <h3>Hello World!!</h3>
-        <p>I am a React Developer {props.name}</p>
+            <p>I am a React Developer {props.name}</p>
         </div>
     );
 };
